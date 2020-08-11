cc70a011a53c14dd9b625c16e0aad3346f0a20ba
devhrs <devhrs239@gmail.com>
2020-08-11 17:16:07 +0430

    replace navbar with user profile AND edited width ALL page to 79.99% userprofile is sidenav with 20% width

--- sa_chat_app/admin.py
+++ sa_chat_app/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Chat
 
-
-@admin.register(Chat)
-class ChatAdmin(admin.ModelAdmin):
-    pass
+# This
+admin.site.register(Chat)
+#   OR
+# @admin.register(Chat)
+# class ChatAdmin(admin.ModelAdmin):
+#     pass
